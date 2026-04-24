103f6b32a03eb82c02aa3f477b06003848700f40
trt1e <vilhelm.grill@gmail.com>
2026-04-24 10:10:47 +0200

    added warning on start

--- js/cookies.js
+++ js/cookies.js
@@ -1,10 +1,15 @@
 let cookiesStatus = localStorage.getItem("cookies-asked");
+let warningStatus = localStorage.getItem("warning-asked")
 
 window.addEventListener("DOMContentLoaded", () => {
     if (!cookiesStatus) { // activates on first load but not after
         const cookiesPopup = document.getElementById("cookies");
         cookiesPopup.style.display = "flex";
-    };    
+    };
+    if (!warningStatus) { // activates on first load but not after
+        const warningPopup = document.getElementById("START_WARNING");
+        warningPopup.style.display = "block";
+    };
 });
 
 function accept_cookies() {
@@ -12,6 +17,12 @@ function accept_cookies() {
     cookiesPopup.style.display = "none";
     localStorage.setItem("cookies-asked", true);
     location.reload();
+};
+function accept_warning() {
+    const warningPopup = document.getElementById("START_WARNING");
+    warningPopup.style.display = "none";
+    localStorage.setItem("warning-asked", true);
+    console.log(localStorage)
 };
 
 function review_cookies() {
